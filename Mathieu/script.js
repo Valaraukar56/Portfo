55c27f747281777c50d418413a1106eedcae5564
DESKTOP-F2NP97E\Doudo <mathieu.piris@outlook.fr>
2025-02-15 01:25:40 +0100

    improve sidebar and HTML/CSS/JS logos

--- Mathieu/script.js
+++ Mathieu/script.js
@@ -2,11 +2,15 @@ function toggleNav() {
   let sidebar = document.querySelector(".sidebar");
   let openbtn = document.querySelector(".openbtn");
 
-  if (sidebar.style.width === "250px") {
-      sidebar.style.width = "0";
-      openbtn.style.display = "block"; // affiche le boutton
+  if (sidebar.dataset.open) {
+    sidebar.style.transform = "translate(-100%)";
+    openbtn.style.display = "block"; // affiche le boutton
+    sidebar.removeAttribute("data-open");
+    sidebar.style.opacity = 0;
   } else {
-      sidebar.style.width = "250px";
-      openbtn.style.display = "none"; // cache le boutton
+    sidebar.style.transform = "translate(0)";
+    openbtn.style.display = "none"; // cache le boutton
+    sidebar.dataset.open = "true";
+    sidebar.style.opacity = 1;
   }
 }
